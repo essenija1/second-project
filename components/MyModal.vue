45d262c2712c89c6e90d99730fc79275c7a8cfb6
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-24 11:38:40 +0300

    Create MyModal.vue

---
+++ components/MyModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="modal-wrapper">
+        <div class="modal-content">
+            <slot/>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+       name: 'MyModal',
+    }
+</script>
+<style scoped>
+.modal-wrapper {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 0;
+    background: rgba(0,0,0, .90);
+}
+</style>
