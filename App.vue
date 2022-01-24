2b7ccc0385999a38c566b4182ee483d28e4afbc2
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-24 11:39:05 +0300

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+<div class="my-div" style="dorder: 1px solid black; min-height: 50px; mergin: 10px 0;"></div>
+<HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
