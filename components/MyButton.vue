65bd88c713fa3b9ba53e7d88281974b2a4e90d55
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-24 11:38:04 +0300

    Create MyButton.vue

---
+++ components/MyButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button @clickHandler="">Button 1</button>
+        
+    </div>
+</template>
+<<script>
+export default {
+   name: "MyButton",
+   emits: {
+    'custom-event-1':null,
+    'custom-event-2'(payload) {
+        return !!(payload.name && payload.role)
+    }
+    },
+   methods: {
+       clickHandler() {
+           this.$emit('custom-event-1', 'data1');
+           this.$emit('custom-event-2', {
+               name: 'Olga',
+               role: "me"
+           });
+       }
+   },
+}
+</script>
+<style>
+</style>
